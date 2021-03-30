4ae8b4096c88027bd64523ac9728ed5015f85ebc
Lee JungGu <whatmyname@naver.com>
2021-03-30 18:48:15 +0900

    jsonwebtoken을 이용하여 로그인시 쿠키에 토큰 저장

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cookieParser = require("cookie-parser");
 const { User } = require("./models/User");
 
 const config = require("./config/key");
@@ -9,6 +10,8 @@ const config = require("./config/key");
 app.use(express.json());
 //application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -29,11 +32,42 @@ app.post("/register", (req, res) => {
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터베이스에 넣어줌.
   const user = new User(req.body);
-
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
   });
+});
+
+app.post("/login", (req, res) => {
+  //요청된 이메일이 데이터베이스에 있는지 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        massage: "해당 이메일 유저가 없습니다.",
+      });
+    }
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          massage: "비밀번호가 틀렸습니다.",
+        });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        //토큰을 쿠키에 저장
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+
+  //비밀번호까지 맞다면 토큰 생성
 });
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +33,44 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    //비밀번호 암호화
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, callBack) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return callBack(err);
+    callBack(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (callBack) {
+  const user = this;
+  //jsonwebtoken을 이용해서 웹토큰 생성
+  const token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+  user.save((err, user) => {
+    if (err) return callBack(err);
+    return callBack(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
