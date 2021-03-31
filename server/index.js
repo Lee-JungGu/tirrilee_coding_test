e5db205d90ab88e6de65b8d5921368d79fe3926b
Lee JungGu <whatmyname@naver.com>
2021-04-01 08:38:40 +0900

    .

--- server/index.js
+++ server/index.js
@@ -111,7 +111,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT ? process.env.PORT : 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Example app listening at ${PORT}`);
