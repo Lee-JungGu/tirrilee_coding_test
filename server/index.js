3d4a1a8692e857d1865fd8a329dc0fbb6452fb59
Lee JungGu <whatmyname@naver.com>
2021-04-01 03:20:49 +0900

    오타수정

--- server/index.js
+++ server/index.js
@@ -110,7 +110,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 
-  const port = process.env.PROT || 5000;
+  const port = process.env.PORT || 5000;
 
   app.listen(port, () => {
     console.log(`Example app listening at ${port}`);
