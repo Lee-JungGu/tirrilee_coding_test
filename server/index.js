5beb924dc2de83de61bda669a7bc601ff9984b4c
Lee JungGu <whatmyname@naver.com>
2021-04-01 03:54:56 +0900

    .

--- server/index.js
+++ server/index.js
@@ -107,9 +107,10 @@ if (process.env.NODE_ENV === "production") {
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve("../client", "build", "index.html"));
   });
 }
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
