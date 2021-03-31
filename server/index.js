257219079425368930090aa9217ad41606b45600
Lee JungGu <whatmyname@naver.com>
2021-04-01 03:31:50 +0900

    오타수정

--- server/index.js
+++ server/index.js
@@ -109,10 +109,9 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
-
-  const port = process.env.PORT || 5000;
-
-  app.listen(port, () => {
-    console.log(`Example app listening at ${port}`);
-  });
 }
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`);
+});
